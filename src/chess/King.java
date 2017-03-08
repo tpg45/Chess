c55daab44b867f1949c063d405de4f7d55b447c4
nprez <nick_prezioso@hotmail.com>
2017-03-07 23:22:35 -0500

    most of King logic written

--- src/chess/King.java
+++ src/chess/King.java
@@ -8,9 +8,43 @@ public class King extends Piece{
 	
 	@Override
 	public boolean canMove(int x, int y){
+		if(x<0 || x>7 || y<0 || y>7)
+			return false;
 		boolean normalMove = (this.x!=x || this.y!=y) && (this.x-x<=1 && this.x-x>=-1) && (this.y-y<=1 && this.y-y>=-1);
-		boolean castle = !hasMoved && y==0 && Math.abs(this.x-x)==2;
-		return normalMove || castle;
+		if(normalMove){
+			boolean noBlocks = Chess.board[x][y].toString().charAt(0)!=color;
+			return normalMove && noBlocks;
+		}
+		else{
+			boolean castle = !hasMoved && (this.y==0 || this.y==7) && this.y==y && Math.abs(this.x-x)==2;
+			boolean noBlocks = true;
+			boolean rookNotMoved = false;
+			//King's side castle
+			if(x>this.x){
+				for(int i = this.x; i<7; i++){
+					if(Chess.board[i][y].toString().charAt(0)=='w' || Chess.board[i][y].toString().charAt(0)=='b'){
+						noBlocks=false;
+						break;
+					}
+				}
+				if(!Chess.board[7][y].hasMoved)
+					rookNotMoved=true;
+			}
+			//Queen's side castle
+			else{
+				for(int i = 1; i<this.x; i++){
+					if(Chess.board[i][y].toString().charAt(0)=='w' || Chess.board[i][y].toString().charAt(0)=='b'){
+						noBlocks=false;
+						break;
+					}
+				}
+				if(!Chess.board[0][y].hasMoved)
+					rookNotMoved=true;
+			}
+			//fix later
+			boolean notUnderAttack = true;
+			return castle && rookNotMoved && noBlocks && notUnderAttack;
+		}
 	}
 	
 	@Override
--- src/chess/Pawn.java
+++ src/chess/Pawn.java
@@ -5,5 +5,15 @@ public class Pawn extends Piece{
 	public Pawn(int x, int y, char color) {
 		super(x, y, color);
 	}
+	
+	@Override
+	public boolean canMove(int x, int y){
+		return this.x==x && (Math.abs(this.y-y)==1 || (!hasMoved && Math.abs(this.y-y)==2));
+	}
+	
+	@Override
+	public String toString(){
+		return color+"p";
+	}
 
 }
