00855beece6a7045e8dc9fd62311eee60e06a48b
tpg45 <tpg45@scarletmail.rutgers.edu>
2017-03-07 22:27:12 -0500

    added printBoard and scanner for inputs

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Scanner;
+
 public class Chess {
 	static Piece[][] board = new Piece[8][8];
 	
@@ -31,22 +33,45 @@ public class Chess {
 			board[6][i] = new Pawn(6,i,'b');
 		}
 		
-		board[7][0] = new Rook(7,0,'w');
-		board[7][1] = new Knight(7,1,'w');
-		board[7][2] = new Bishop(7,2,'w');
-		board[7][3] = new Queen(7,3,'w');
-		board[7][4] = new King(7,4,'w');
-		board[7][5] = new Bishop(7,5,'w');
-		board[7][6] = new Knight(7,6,'w');
-		board[7][7] = new Rook(7,7,'w');
+		board[7][0] = new Rook(7,0,'b');
+		board[7][1] = new Knight(7,1,'b');
+		board[7][2] = new Bishop(7,2,'b');
+		board[7][3] = new Queen(7,3,'b');
+		board[7][4] = new King(7,4,'b');
+		board[7][5] = new Bishop(7,5,'b');
+		board[7][6] = new Knight(7,6,'b');
+		board[7][7] = new Rook(7,7,'b');
+	}
+	
+	public static void printBoard(){
+		for(int i = 7;i>=0;i--){
+			for(int j = 0;j<=7;j++){
+				System.out.print(board[i][j].toString()+" ");
+			}
+			
+			System.out.println(i+1);
+		}
+		System.out.println(" a b c d e f g h");
+		System.out.println();
 	}
 	
 	public static void main(String[] args) {
 		
 		initBoard();
 		
+		boolean currentPlayer = true;		//true=white
+		while(true){
+			printBoard();
+			Scanner scanner = new Scanner(System.in);
+			if(currentPlayer)
+				System.out.print("White's move: ");
+			else
+				System.out.print("Black's move: ");
+			String input = scanner.next();
+			System.out.println('\n');
+		}
 		
 		
-		return;
+		
 	}
 }
